667886fb49dc75b693a96f262fb5676421332139
Noah Fajarda <noahfajarda@gmail.com>
2023-10-21 14:47:05 -0700

    removed button from protected page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,12 @@
-import { Button } from "@/components/ui/button";
-
 // this is the root url
-export default function Home() {
-  return (
-    <div className="p-4">
-      {/* LOOK AT DOCUMENTATION FOR shadcn-ui */}
-      {/* size: adjust size */}
-      {/* variant: adjust color */}
-      <Button size="default" variant="destructive">
-        Click Me
-      </Button>
-    </div>
-  );
+export default function SetupPage() {
+  return <div className="p-4">This is a protected route!</div>;
 }
+
+// {/* LOOK AT DOCUMENTATION FOR shadcn-ui */}
+// import { Button } from "@/components/ui/button";
+//       {/* size: adjust size */}
+//       {/* variant: adjust color */}
+//       <Button size="default" variant="destructive">
+//         Click Me
+//       </Button>
