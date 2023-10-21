c6f03f50f3cf09ad931103c900c3d3d0547685be
Noah Fajarda <noahfajarda@gmail.com>
2023-10-21 14:41:07 -0700

    component allows for 'sign-in with Google'

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignIn } from "@clerk/nextjs";
 
+// allows to SIGN-IN WITH GOOGLE
+
 export default function Page() {
   // built-in sign-in component powered by 'clerk'
   return <SignIn />;
--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignUp } from "@clerk/nextjs";
 
+// allows to SIGN-IN WITH GOOGLE
+
 export default function Page() {
   // built-in sign-in component powered by 'clerk'
   return <SignUp />;
